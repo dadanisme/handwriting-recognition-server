b78613dadab93aeb59d7ec9dd126506ee78f8e41
dadanisme <dadanisme@upi.edu>
2022-06-05 14:07:47 +0700

    first

--- vision.py
+++
@@ -1,7 +0,0 @@
-from setup import *
-
-
-path = './input/handwritten.jpeg'
-response = detect_handwritten_ocr(path)
-# print(response)
-display_detected_handwritten(path, response, 'handwritten.jpeg')
